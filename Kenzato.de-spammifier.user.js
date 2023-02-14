255a8af3e7c4e9778340ba1f8a122ed686ecf98d
CttCJim <31054746+CttCJim@users.noreply.github.com>
2023-02-14 12:28:39 -0700

    v0.4.0
    Added version checking

--- Kenzato.de-spammifier.user.js
+++ Kenzato.de-spammifier.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Kenzato.de-spammifier
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.4.0
 // @description  Remove spam from Kenzato Booru
 // @author       CttCJim
 // @match        https://kenzato.uk/*
 // @grant        GM_notification
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kenzato.uk
-
-// @grant        none
+// @grant        GM_notification
 // ==/UserScript==
 
 (async function() {
@@ -31,27 +30,53 @@
             `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
         )
         .then (d => d.json ())
-        .then (d =>
-               fetch (
-            `https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`
-        )
-              )
+        .then (d => fetch (`https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`))
         .then (d => d.json ())
-        .then (d => JSON.parse (atob (d.content)));
+        .then (d => {
+            if(isJson(atob (d.content))) {
+               return JSON.parse (atob (d.content));
+            } else {
+               return (atob (d.content));
+            }
+            });
 
         return data;
     }
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
     //check for updates
-    const latest = await getgit('CttCJim','Kenzato-de-spammifier','Kenzato de-spammifier.user.js');
+    const latest = await getgit('CttCJim','Kenzato-de-spammifier','Kenzato.de-spammifier.user.js');
     const ver = GM_info.script.version;
-    console.log(ver);
-    console.log(latest);
+   // console.log(ver);
+   // console.log(latest);
+    var newver;
+    var lines = latest.split(/\r?\n|\r|\n/g);
+    for(var i=0;i<lines.length;i++) {
+        if(lines[i].indexOf("@version")!=-1) {
+            var theline = lines[i];
+            var stpos = theline.indexOf(".")-1;
+            newver = theline.substr(stpos);
+        }
+    }
+    if(newver!=ver) {
+        GM_notification({
+            text: "Warning: you are using v"+ver+" of CttCJim's Kenzato de-spammifier. The latest version is "+newver+". Click here to go to the download page.",
+            title: "Version Mismatch",
+            image: "https://i.imgur.com/q7paNPN.png",
+            onclick: () => {window.open("https://github.com/CttCJim/Kenzato-de-spammifier/releases/", '_blank').focus();}
+        });
+    }
+
 
     //your list is now combined with the community list
     const blacklist = myblacklist.concat(JSON.parse(await getgit('CttCJim','Kenzato-de-spammifier','blacklist_url.json')));
-    console.log("BLACKLIST:");
-    console.log(blacklist);
-    for(var i=0;i<blacklist.length;i++) {
+    for(i=0;i<blacklist.length;i++) {
         $('a[href$="'+blacklist[i]+'"]').closest(".c8").remove(); //search by URL
         //$("a:contains('"+blacklist[i]+"')").closest(".c8").remove(); //search by name
     }
